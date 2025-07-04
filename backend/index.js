9975cfe63b9de176a4598ab4e8fd0ed73b4b99a3
Umesh Raju <roronoazoro98760@gmail.com>
2025-07-04 16:50:28 +0530

    fix: update CORS to allow Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000', // Your frontend URL
+  origin: ['http://localhost:3000','https://b2b-tender-platform-three.vercel.app'], // Your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
